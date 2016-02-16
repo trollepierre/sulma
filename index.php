6a6a06f853bf5a7411212bfeeb8edcff38d6c30d
trollepierre <trolle.pierre@gmail.com>
2016-02-16 15:52:46 +0100

    Imports basiques

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE HTML>
+<!--
+    /*
+    * Sulma 
+    * https://github.com/trollepierre/sulma
+    * Copyright 2015, Pierre Trolle
+    * http://pierre.recontact.me
+    */
+-->
+<html lang="en"> 
+<head>
+    <!--[if IE]> <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> <![endif]-->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="title" content="Gracias Sulma"/>
+    <meta name="description" content="Muchas Gracias por mi ami Sulma">
+    <meta name="author" content="Pierre Trollé">
+    <link rel="icon" href="">
+    <title>Gracias Sulma!</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <!--   <link rel="stylesheet" href="../1external/bootstrap.min.css"> -->
+    <link rel="stylesheet" href="css/bootstrap-datetimepicker.css">
+    <link rel="stylesheet" href="css/blog.css">
+</head>
+<body>
+   
+    <div class="container">
+
+        <div class="blog-header">
+            <h1 class="blog-title">Merci Sulma !</h1>
+            <p class="lead blog-description">Titre du message</p>
+            <p> Un texte de voeux, de remerciement, un message à faire passer ! <br/>
+                C'est le moment d'admirer les images que j'ai choisies pour l'occasion.
+            </p>
+        </div>
+        <div class="row">
+
+            <div class="col-sm-3"> <img src="img/bigar.png" ></img></div>
+            <div class="col-sm-6"> <img src="img/bigar.png" ></img></div>
+            <div class="col-sm-3"> <img src="img/bigar.png" ></img></div>
+        
+     
+            </div><!-- /.blog-main -->
+            
+        
+
+            </div><!-- /.row -->
+
+        </div><!-- /.container -->
+
+
+        <!-- <script type="text/javascript" src="../1external/bootstrap.min.js"></script> -->
+        <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <!-- <script type="text/javascript" src="../1external/jquery-11.1.3.min.js"></script> -->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> 
+        
+    </body>
+    </html>
